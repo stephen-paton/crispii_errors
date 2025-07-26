3f7d6a1a685e735531d4ee33f068a8cd8cf7d623
Stephen Paton <69050696+stephen-paton@users.noreply.github.com>
2025-07-26 23:09:41 +0100

    ImpossibleOperationError type added

---
+++ src/impossible_operation_error.rs
@@ -0,0 +1,50 @@
+use std::{error::Error, fmt};
+
+use crate::CrispiiError;
+
+/// To indicate to the caller of a function that the operation that they attempted to perform using the function was impossible, and an explanation as to why it was impossible
+/// ```
+///     use crispii_errors::{ImpossibleOperationError, CrispiiError};
+/// 
+///     fn add_one(num: u8) -> Result<u8, Box<dyn CrispiiError>> {
+///         if num == u8::MAX {
+///             return Err(Box::new(ImpossibleOperationError::new("'num' is already equal to the maximum possible u8 value")));
+///         }
+///         
+///         Ok(num + 1)
+///     }
+/// ```
+#[derive(Debug)]
+pub struct ImpossibleOperationError {
+    explanation: String,
+}
+
+impl ImpossibleOperationError {
+    pub fn new(explanation: &str) -> Self {
+        Self {
+            explanation: explanation.to_string(),
+        }
+    }
+}
+
+impl Error for ImpossibleOperationError {}
+
+impl CrispiiError for ImpossibleOperationError {}
+
+impl fmt::Display for ImpossibleOperationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}!", self.explanation)
+    }    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn error_message() {
+        let error = ImpossibleOperationError::new("'num' is already equal to the maximum possible u8 value");
+
+        assert_eq!(error.to_string(), "'num' is already equal to the maximum possible u8 value!");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,6 @@ pub use crispii_error::CrispiiError;
 
 mod invalid_argument_error;
 pub use invalid_argument_error::InvalidArgumentError;
+
+mod impossible_operation_error;
+pub use impossible_operation_error::ImpossibleOperationError;
